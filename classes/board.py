d4f3aa9e8daa42bef0a92149201f34efeb16d145
mattkoler <mkoler@gmail.com>
2017-10-13 16:25:40 -0700

    added deck
    added deck and deck draw methods

--- classes/board.py
+++ classes/board.py
@@ -1,13 +1,30 @@
+from random import shuffle
+
 class Board:
     
     #tiles should be a list of tile class objects
     def __init__(self, tiles):
         self.tiles = tiles
-        self.row_1 = tiles[:4]
-        self.row_2 = tiles[4:8]
-        self.row_3 = tiles[8:12]
-        self.row_4 = tiles[12:]
-    
+        self.deck = ["Gain 1 resource",] * 4 + \
+            ["Gain 5 Lira",] * 4 + \
+            ["Carry out Sultan's Palace action an additional time",] * 2 + \
+            ["Carry out Post Office action an additional time",] * 2 + \
+            ["Carry out Gemstone Dealer action an additional time",] * 2 + \
+            ["Return your family member to the Police Station and collect the reward",] * 2 + \
+            ["Move 0 this turn instead of 1-2 (take the tile action still)",] * 2 + \
+            ["Move 3-4 this turn instead of 1-2",] * 4 + \
+            ["Before or immediately after movement, return 1 assistant to your stack",] * 2 + \
+            ["You may sell any goods to meet the Small Market demand"] *2
+        shuffle(self.deck)
+
+    def deck_draw(self):
+        if len(self.deck) <= 0:
+            self.deck = tile.caravansery.get_discard() #TODO: fix call to caravansery
+            if len(self.deck <= 0):
+                return False
+            shuffle(self.deck)
+        return self.deck.pop()
+
     def print_board(self):
         print('+-------------------+-------------------+-------------------+-------------------+')
         for row in [self.tiles[x:x+4] for x in range(0,16,4)]:
