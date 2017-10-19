06a648422de63f496287252658d3f9d255dd3653
mattkoler <mkoler@gmail.com>
2017-10-19 16:18:10 -0700

    fix typo

--- source/board.py
+++ source/board.py
@@ -29,6 +29,6 @@ class Board:
         print('+-------------------+-------------------+-------------------+-------------------+')
         for row in [self.tiles[x:x+4] for x in range(0,16,4)]:
             print('| {:^18}| {:^18}| {:^18}| {:^18}|'.format(*(n.name for n in row)))
-            print('| M: {0:<5} A: {4:<5} | M: {1:<5} A: {5:<5} | M: {2:<5} A: {6:<5} | M: {3:<5} A: {7:<5} |'.format(*(n.get_merchants() for n in row),*(n.get_assistants() for n in row)))
+            #print('| M: {0:<5} A: {4:<5} | M: {1:<5} A: {5:<5} | M: {2:<5} A: {6:<5} | M: {3:<5} A: {7:<5} |'.format(*(n.get_merchants() for n in row),*(n.get_assistants() for n in row)))
             #print('| F: {} G: {} S: {} |')
             print('+-------------------+-------------------+-------------------+-------------------+')
