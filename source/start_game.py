9cc561606a41cb421ff1898dec2964c185e93357
mattkoler <mkoler@gmail.com>
2017-10-25 13:48:12 -0700

    minor changes
    changed a few things to work better with the sanitised input functions and expanded the function to create a board

--- source/start_game.py
+++ source/start_game.py
@@ -1,12 +1,19 @@
-
+from sanitised_inputs import input_from_list, input_int_range
+from gen_boardv2 import generate_board
+from board import Board
 
 def start_game():
-    num_players = int(input("How many players? (2-5): "))
-    colors = ['(W)hite', '(G)reen', '(R)ed', '(B)lue', '(P)urple']
+    num_players = input_int_range("How many players? ", 2, 5)
+    colors = ['White', 'Green', 'Red', 'Blue', 'Purple']
     p_colors = []
-    for _ in range(num_players):
-        choice = input("What color could you like to choose? {} :".format(str(colors)))
+    for i in range(num_players):
+        choice = input_from_list("What color would you like for player {}? ".format(i+1), colors)
         p_colors.append(choice)
-    print("You chose {} players with the colors {}.".format(num_players,str(p_colors)))
+        colors.remove(choice)
+    print("You chose {} players with the colors {}.".format(num_players, str(p_colors)))
+    gen_options = ['default', 'shortest', 'farthest', 'random', 'balanced']
+    gen = input_from_list("How would you like to generate the board? ", gen_options)
+    game_board = Board(generate_board(num_players, gen))
+    game_board.print_board()
 
 start_game()
