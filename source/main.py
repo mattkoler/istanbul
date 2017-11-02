dd98288d35fd25bdeb6376c85678f1ced749abc2
mattkoler <mkoler@gmail.com>
2017-11-02 15:42:32 -0700

    minor

--- source/main.py
+++ source/main.py
@@ -1,7 +1,7 @@
-import board, gen_boardv2, player_wagon, tile
+import board, gen_boardv2, player_wagon, tile, start_game, take_turn
 
-tiles = gen_boardv2.generate_board(5,'balanced')
+game_board, players = start_game.start_game()
 
-game_board = board.Board(tiles)
+for player in players:
+    take_turn.take_turn(player, game_board)
 
-game_board.print_board()
