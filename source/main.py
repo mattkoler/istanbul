26d0995a424ff7b3aaa9658a14d413b8edd77c03
mattkoler <mkoler@gmail.com>
2017-11-03 16:05:27 -0700

    added players
    passing players to the take_turn function it is easier to modify other players' info

--- source/main.py
+++ source/main.py
@@ -1,7 +1,8 @@
-import board, gen_boardv2, player_wagon, tile, start_game, take_turn
+import board, gen_boardv2, player_wagon, tile, start_game, take_turn, copy
 
 game_board, players = start_game.start_game()
 
 for player in players:
-    take_turn.take_turn(player, game_board)
+    take_turn.take_turn(player, players game_board)
 
+game_board.print_board()
